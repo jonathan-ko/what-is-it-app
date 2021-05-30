7f703667f793845c0547c00c256150e8818bc2e2
Jonathan Ko <Jonathan.ko@gmail.com>
2021-05-30 12:22:06 -0700

    mix n match

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+  <head>
+    <!-- Load the latest version of TensorFlow.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet"></script>
+  </head>
+  <body>
+    <div id="console"></div>
+    <!-- Add an image that we will use to test -->
+    <img id="img" crossorigin src="https://i.imgur.com/JlUvsxa.jpg" width="227" height="227"/>
+    <!-- Load index.js after the content of the page -->
+    <script src="index.js"></script>
+  </body>
     </div>
   );
 }
